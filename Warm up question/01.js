1a90879ef4bec64b4aa49c352a048157a0e10e8c
Sahil Kumar <sahilkumar7369@gmail.com>
2026-08-03 23:00:33 +0530

    01 question complete

--- Warm up question/01.js
+++ Warm up question/01.js
@@ -1,1 +1,14 @@
-// hello ji
+// Question no: 01
+// Create a function that return the last element of an array.
+
+
+function getLastElement(arr){
+    return arr[arr.length - 1];
+}
+
+let myArray = [1,5,3,7,9];
+
+let LastElement = getLastElement(myArray);
+console.log(LastElement);  // output: 9
+
+
