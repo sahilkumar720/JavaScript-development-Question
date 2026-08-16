df6309b82221414e639485b3ee7e85a354a62f53
Sahil Kumar <sahilkumar7369@gmail.com>
2026-08-16 12:21:43 +0530

    03 question complete

--- Warm up question/03.js
+++ Warm up question/03.js
@@ -1,13 +1,6 @@
 //question no : 2
-// find the combination of two array.
+// generate a random integer between 0 to 18.
 
 
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let arr3 = ['a', 'b']
-
-// let combinationArr = arr1.concat(arr2, arr3)
-
-
-let combinationArr = [...arr1, ...arr2, ...arr3]
-console.log(combinationArr);
+let randomNumber = Math.floor(Math.random()*19);
+console.log(randomNumber);
