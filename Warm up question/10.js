4aa4ec4a40cae04e5e180aa51348c5b1002d1891
Sahil Kumar <sahilkumar7369@gmail.com>
2026-08-27 18:39:41 +0530

    10 question complete

---
+++ Warm up question/10.js
@@ -0,0 +1,10 @@
+// question no: 10
+// Create an array of numbers and double each value using map().
+
+let arr = [1,5,2,3,8];
+
+let finalArr = arr.map(num => {
+    return num*2
+})
+
+console.log(finalArr);
