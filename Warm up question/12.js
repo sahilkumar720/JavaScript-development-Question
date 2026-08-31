49dff8386aadacdacee2013442af740ea6e2daa8
Sahil Kumar <sahilkumar7369@gmail.com>
2026-08-31 21:52:45 +0530

    12 question complete

---
+++ Warm up question/12.js
@@ -0,0 +1,6 @@
+// question no: 12
+// write a function to flatten a nested array (one level deep)(e.g: [1,[2,3],4]=>[1,2,3,4])
+
+let arr = [1,2,3,4, ['a', 'b', 'c'], 5, 6];
+
+console.log(arr.flat(1));
