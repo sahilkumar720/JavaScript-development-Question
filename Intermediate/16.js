e609ae892c96c12011524d29e30d81bbd0eb068f
Sahil Kumar <sahilkumar7369@gmail.com>
2026-09-04 11:33:25 +0530

    16 question complete

---
+++ Intermediate/16.js
@@ -0,0 +1,13 @@
+// Question no:01/16
+// What's the difference between parseint and Number()?
+
+// console.log(parseInt("123.3453"));
+// console.log(Number("123.3453"));
+
+
+// console.log(parseInt("123.234px"));
+// console.log(Number("123.234px"));
+
+
+console.log(parseInt("110, 16"));
+console.log(Number("10"));
