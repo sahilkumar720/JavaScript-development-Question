a7d8d4fe8d4f6b9e48c4c40518958da22d5f99d0
Sahil Kumar <sahilkumar7369@gmail.com>
2026-09-17 16:49:23 +0530

    28 question complete

---
+++ Intermediate/28.js
@@ -0,0 +1,28 @@
+// 28.****
+
+
+// let str = "Manas";
+
+// function getOccurence(string){
+
+//     let obj = {};
+
+//     string.split('').forEach(letter => {
+//         if(obj.hasOwnProperty(letter)){
+//            obj[letter] ++
+//         }else{
+//             obj[letter] = 1;
+//         }
+//     });
+
+//     // for (let key in obj){
+//     //     console.log(`${key} apka ${obj[key]} bar aaya hai`)
+//     // }
+
+//     let arr = Object.entries(obj)
+//     arr.forEach(entryArr =>{
+//         console.log(`${entryArr[0]} apka ${entryArr[1]} baar aaya hai`)
+//     })
+// }
+
+// getOccurence(str)
